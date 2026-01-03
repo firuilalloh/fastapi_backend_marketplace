783fe5aa7555f92f6c0bcbda90e53378bbbdf47c
firuialloh <firuilalloh@gmail.com>
2026-01-03 09:13:42 +0700

    fixing refresh token and add admin check in update and delete products

--- app/routers/products.py
+++ app/routers/products.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter, HTTPException
+from typing import Annotated
+from fastapi import APIRouter, HTTPException, Depends
 from ..models.product_model import Product, productResponse, productResponseId, productUpdate, productUpdateResponse
 from ..services.product_service import s_get_all_products as sgap, s_get_product_by_id as sgpb, s_update_product as sup, s_delete_product as sdp
+from ..services.authentication_service import get_current_user, check_is_admin
+from ..models.auth_model import User
 
 router = APIRouter(prefix="/api/products", tags=["Products"])
 
@@ -9,15 +12,15 @@ def r_get_all_products():
     return sgap()
 
 @router.get("/{id}", response_model=productResponseId)
-def r_get_product_by_id(id: int):
+def r_get_product_by_id(id: int, current_user: Annotated[User, Depends(get_current_user)]):
     return sgpb(id)
     
 @router.delete("/delete/{id}")
-def r_delete_product(id: int):
+def r_delete_product(id: int, admin_user: Annotated[User, Depends(check_is_admin)]):
     return sdp(id)
 
 @router.patch("/update/{id}", response_model=productUpdateResponse)
-def r_update_product(id: int, product_update: productUpdate):
+def r_update_product(id: int, product_update: productUpdate, admin_user: Annotated[User, Depends(check_is_admin)]):
 
     update_data = product_update.model_dump(exclude_unset=True)
 
