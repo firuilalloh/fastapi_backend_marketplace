7fd22b6c30ce59deff7a8a4a153314dabb9b8819
firuialloh <firuilalloh@gmail.com>
2026-01-08 12:13:32 +0700

    Update CORS Origin

--- main.py
+++ main.py
@@ -39,6 +39,7 @@ def authenticate_dev(credentials: HTTPBasicCredentials = Depends(security)):
 origins = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "https://fastapi-backend-marketplace.vercel.app/",
 ]
 
 app.add_middleware(
